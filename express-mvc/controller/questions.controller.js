eb208321acd2ea722813442f1bdefa6ad49d387c
hafidz <hafidzzpermana09@gmail.com>
2017-11-22 19:34:29 +0700

    add callback function that only authorized user can delete questions

--- express-mvc/controller/questions.controller.js
+++ express-mvc/controller/questions.controller.js
@@ -67,82 +67,17 @@ module.exports = {
 	},
 
 	deleteAQuestion: (req, res, next) => {
+		const token = req.headers.authorization || req.body.token
+    	const user = helpers.decodeToken(token)
+
+    	if (user) {
 		Question.findOneAndRemove({ _id: req.params.qID })
 			.then(function(question) {
 				res.send(question);
 			})
 			.catch(err => console.error(err));
-	},
-
-	//Answer
-	postAnAnswer: (req, res, next) => {
-		const token = req.headers.authorization || req.body.token
-    	const user = helpers.decodeToken(token)
-
-    	if (user) {
-		const newAnswer = {
-			content: req.body.content,
-			by: user._id
-		};
-		Question.update(
-			{ _id: req.params.qID },
-			{ $push: { answers: newAnswer } },
-			function(err) {
-				if (err) {
-					res.send(err);
-				} else {
-					res.send({
-						data: newAnswer
-					});
-				}
-			}
-		)	
 	} else {
-      // NOTIFY IF USER TOKEN IS INVALID
-      res.send({ message: "User token is invalid" })
-    }
- },
-                                                     
-	putAnAnswer: (req, res, next) => {
-		Question.findOneAndUpdate(
-			{_id: req.body.aID, 'answers._id': req.params.aID},
-			{'answers.$.content': req.body.content}
-			)
-			if (err) {
-				res.send(err)
-			} else {
-				res.send(answer)
-			}
-	},
-
-	deleteAnAnswer: (req, res, next) => {
-		Question.findOneAndUpdate(
-			{ _id: req.params.aID },
-			{ $pull: { answers: { _id: req.params.aID, 'user': req.user._id } } }
-			).then(function(answer) {
-			res.send(answer)
-		})
-	},
-
-	//QUESTION & ANSWER VOTE
-	voteUpAQuestion: (req, res, next) => {
-		// Question.findOneAndUpdate(
-		// 	{ _id: req.params.qID },
-		// 	{ votes: }
-		// 	)
-		// 	}
-		// }
-	},
-
-	// voteDownAQuestion: (req, res, next) => {
-
-	// },
-
-	// voteUpAnAnswer: (req, res, next) => {
-
-	// },
-
-	// voteDownAnAnswer: (req, res, next) => {
-
-	// }
+			res.send({ message: "Not authorized t delete this question" });
+		}
+	}
 };
