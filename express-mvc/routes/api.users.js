c4e0826d221d2324c2212003a9cfb38489167664
hafidz <hafidzzpermana09@gmail.com>
2017-11-22 12:45:58 +0700

    modify route for specific user

--- express-mvc/routes/api.users.js
+++ express-mvc/routes/api.users.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 const Users = require("../controller/users.controller")
 
-router.get("/", Users.getAllUsers)
-router.get("/:id", Users.getAUsers)
+router.get("/", Users.getAllUsers) // api/users/
+router.get("/:uID", Users.getAUsers) // api/users/:uID
+router.get("/:uID", Users.destroy) // api/users/:uID
 
 module.exports = router
